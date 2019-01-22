ab088ea0fa7e25743385240a4cd61a7ddbaa8cef
hir-suzuki <hir-suzuki@dip-net.co.jp>
2019-01-22 11:33:36 +0900

    navView入れる

---
+++ pullToClose/NavView.swift
@@ -0,0 +1,14 @@
+//
+//  NavView.swift
+//  pullToClose
+//
+//  Created by hir-suzuki on 2019/01/22.
+//  Copyright © 2019年 hir-suzuki. All rights reserved.
+//
+
+import UIKit
+
+class NavView: UIView {
+    @IBAction func tapCloseButton(_ sender: UIButton) {
+    }
+}
